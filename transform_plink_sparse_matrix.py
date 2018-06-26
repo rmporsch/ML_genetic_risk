64dadbf887c4e0c14dc1e2d3beaad81066c0c81e
Robert Porsch <rmporsch@gmail.com>
2018-06-26 10:24:24 +0800

    allows to specifiy specific chr to transform

--- transform_plink_sparse_matrix.py
+++ transform_plink_sparse_matrix.py
@@ -3,13 +3,15 @@ from wepredict.plink_reader import Genetic_data_read
 from wepredict.data_download import genetic_testdata
 import sys
 
-download_path = sys.arg[2]
-plink_step = sys.arg[1]
+plink_step = sys.argv[1]
+chr = sys.argv[2]
+download_path = sys.argv[3]
+print(sys.argv)
 
 downloader = genetic_testdata(download_path)
 ld_blocks = downloader.download_ldblocks()
 
 genetic_process = Genetic_data_read(plink_step, ld_blocks)
-out = genetic_process.block_iter(10)
+out = genetic_process.block_iter(int(chr))
 for i in out:
     x = i
