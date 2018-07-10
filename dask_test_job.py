a370562a6a455a8d573d036ace90bc8b660b19d8
Robert Milan Porsch <robert.porsch@hku.hk>
2018-07-10 11:03:13 +0800

    PBS dask job for UKB

--- dask_test_job.py
+++ dask_test_job.py
@@ -1,8 +1,14 @@
 """Compute LD-block wise prediction scores."""
 from dask.distributed import Client, LocalCluster
+from dask_jobqueue import PBSCluster
 import pickle
 import numpy as np
+import pandas as pd
 from wepredict.wepredict import wepredict
+from glob import glob
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 def save_pickle(object, path):
     """Save obejct."""
@@ -15,7 +21,7 @@ def load_pickle(path):
 
 
 if __name__ == '__main__':
-    testing = True
+    testing = False
     if testing:
         cluster = LocalCluster()
         cluster.scale(3)
@@ -23,23 +29,57 @@ if __name__ == '__main__':
         print(client)
         folder = 'data/1kg_LD_blocks/'
         alphas = np.arange(0.2, 2, 0.4)
-        monster = wepredict(folder+'/*', cluster, True)
-        phenoype = monster.simulate()
-        phenoype = phenoype.sum(axis=1).values
-        index_valid = np.random.choice(range(len(phenoype)),
+        monster = wepredict(folder+'/*', cluster, False)
+        phenotype = monster.simulate()
+        phenotype = phenotype.sum(axis=1).values
+        index_valid = np.random.choice(range(len(phenotype)),
                                        100, replace=False)
-        mask = np.ones(len(phenoype), dtype=bool)
+        index_file = 'training_validation_index_testing.pickle'
+        if os.path.isfile(index_file):
+            index_valid = pickle.load(open(index_file, 'rb'))
+        else:
+            index_valid = np.random.choice(range(len(phenotype)), 100, replace=False)
+            pickle.dump(index_valid, open(index_file, 'wb'))
+        mask = np.ones(len(phenotype), dtype=bool)
         mask[index_valid] = False
-        save_pickle(index_valid, 'training_validation_index.pickle')
+        outfolder = ''
     else:
-        folder = ''
+        cluster = PBSCluster(processes=2,
+                threads=6, memory="120GB",
+                queue='large',
+                local_directory='$TMPDIR',
+                resource_spec='select=1:ncpus=12:mem=120gb',
+                walltime='12:00:00')
+        print(cluster.job_script())
+        cluster.scale(3)
+        client = Client(cluster)
+        print(client)
+
+        folder = '/home2/groups/pcsham/users/rmporsch/sparse_matrix_ukb_chr10/'
+        pheno_file = '/home/groups2/pcsham/users/rmporsch/simualted_ukb_chr10_phenotype/smaller_pheno.tab'
         alphas = np.arange(0.2, 2, 0.4)
+        monster = wepredict(folder+'/10*', cluster, False)
+        pheno = pd.read_csv(pheno_file, sep='\t')
+        phenotype = pheno['V1'].values
+        files = glob(folder+'/10*')
+        index_file = 'training_valid_index.pickle'
+        if os.path.isfile(index_file):
+            index_valid = pickle.load(open(index_file, 'rb'))
+        else:
+            index_valid = np.random.choice(range(len(phenotype)), len(phenotype)*0.1, replace=False)
+            pickle.dump(index_valid, open(index_file, 'wb'))
+        mask = np.ones(len(phenotype), dtype=bool)
+        mask[index_valid] = False
+        outfolder = '/home2/groups/pcsham/users/rmporsch'
+
 
     models = ['l1', 'l2', 'l0']
     for norm in models:
-        monster.generate_DAG(phenoype, index_valid, alphas, norm)
+        model_save = os.path.join(outfolder, 'models_'+norm+'.pickle')
+        eval_save = os.path.join(outfolder, 'eval_'+norm+'.pickle')
+        monster.generate_DAG(phenotype, index_valid, alphas, norm)
         out = monster.compute()
-        save_pickle(out, 'models_'+norm+'.pickle')
-        oo = monster.evaluat_blocks(out, phenoype[~mask])
-        save_pickle(oo, 'eval_'+norm+'.pickle')
-    cluster.close()
+        save_pickle(out, model_save)
+        oo = monster.evaluat_blocks(out, phenotype[~mask])
+        save_pickle(oo, eval_save)
+    # cluster.close()
