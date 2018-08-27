96a50b2dbe5ec62a7d52a7f02581d5546c2ee7e0
Robert Porsch <rmporsch@gmail.com>
2018-08-27 13:18:19 +0800

    house cleaning

--- testing_regression.py
+++ testing_regression.py
@@ -6,7 +6,8 @@ from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 from wepredict.pytorch_regression import pytorch_linear
 
-
+logging.basicConfig(level=logging.INFO,
+                    filename='testing_pytorch_diff_norms.log')
 lg = logging.getLogger(__name__)
 lg.setLevel(logging.INFO)
 
@@ -50,7 +51,7 @@ def torch_model(x_train, y_train, x_valid, y_valid, alphas, regu):
         predictions.append(i.accu)
     best_lamb = np.argmax(predictions)
     lg.debug('best lambda: %s', best_lamb)
-    return outcome[best_lamb]
+    return outcome[best_lamb], outcome
 
 
 if __name__ == '__main__':
@@ -58,28 +59,35 @@ if __name__ == '__main__':
     p = 10000
     null_prop = 0.99
     y, pred = sim(n, p, null_prop)
-    # alpha_values = np.arange(0.001, 0.01, 0.001)
-    alpha_values = [0.5]
+    alpha_values = np.arange(0.001, 0.01, 0.001)
     lg.info('Number of alphas: %s', alpha_values)
     x_train, x_test, y_train, y_test = train_test_split(pred, y,
                                                         test_size=0.1,
                                                         random_state=42)
     del pred
 
-    regus = ['l0', 'l1']
+    regus = ['l1', 'l0', 'l02']
     sample_limit = np.logspace(2, 4, 10, dtype=int)
     assert sample_limit[-1] == n
     out = dict()
-    out['l1'] = list()
-    out['l0'] = list()
+    for l in regus:
+        out[l] = list()
+    everything = dict()
+    for l in regus:
+        everything[l] = list()
     for s in sample_limit:
         for l in regus:
-            alpha_values = [0.01] if l == 'l0' else [0.01]
-            torch_out = torch_model(x_train[0:int(s), :], y_train[0:int(s)],
+            torch_out, all_models = torch_model(x_train[0:int(s), :], y_train[0:int(s)],
                                     x_test, y_test, alpha_values, regu=l)
             out[l].append(torch_out)
+            everything[l].append(all_models)
             lg.info('Best Torch model %s: %s with %s', l, torch_out.accu, s)
 
     import pickle
-    with open('l1l0_by_samplesize.pickle', 'wb') as f:
+    with open('l1_l0_l02_by_samplesize.pickle', 'wb') as f:
         pickle.dump(out, f)
+    with open('l1_l0_l02_by_samplesize_everything.pickle', 'wb') as f:
+        pickle.dump(everything, f)
+
+    from notify.notify import notify
+    notify('Pytorch model is done!')
