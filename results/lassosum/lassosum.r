b407c4178d1a5ab7c418d63d953ba24cc7ad1875
Robert Milan Porsch <robert.porsch@hku.hk>
2018-11-14 10:28:30 +0800

    updated submit script of tf with lassosum

--- results/lassosum/lassosum.r
+++ results/lassosum/lassosum.r
@@ -3,6 +3,7 @@
 library(lassosum)
 library(data.table)
 args = commandArgs(trailingOnly=TRUE)
+setwd("/home/rmporsch/projects/ML_genetic_risk/results")
 
 ref = args[1]
 dev = args[2]
@@ -10,10 +11,16 @@ sumstat = args[3]
 ld = args[4]
 n = args[5]
 phenotype = args[6]
+pheno.name = args[7]
 
 ld  <- fread(ld)
 ss = fread(sumstat)
-cor = p2cor(p = ss$P, n=n, sign=ss$BETA)
+# ss$P = as.numeric(ss$P, digits=128)
+ss$P[is.infinite(ss$P)] = 1e-128
+print(min(ss$P))
+cor = p2cor(p = as.numeric(ss$P), n=as.integer(n), sign=as.numeric(ss$BETA))
+cor[is.na(cor)] = 0.999999
+
 
 out <- lassosum.pipeline(cor=cor, chr=ss$CHR, pos=ss$BP, 
                          A1=ss$A1,
@@ -24,8 +31,8 @@ dat = read.table(phenotype, head=T)
 dev_fam = read.table(paste0(out$test.bfile, '.fam'))
 names(dev_fam)[1:2] = c('FID', 'IID')
 sub = merge(dev_fam, dat, by=c('FID', 'IID'))
-ssub = sub[,c('FID', 'IID', 'V1')]
-ssub = dat[,c('FID', 'IID', 'V1')]
+ssub = sub[,c('FID', 'IID', pheno.name)]
+ssub = dat[,c('FID', 'IID', pheno.name)]
 v <- validate(out, pheno=ssub)
 output = v$validation.table
 
