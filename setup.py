2d6d922126cf05de9d367c2bbde071a7fc4a97e1
Robert Porsch <rmporsch@gmail.com>
2018-11-23 11:39:07 +0800

    added requirements file and fixed setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='wepredict',
       description='LD-Block prediction',
       author='Robert M. Porsch',
       author_email='rmporsch@gmail.com',
+      install_requires=["bitarray", "matplotlib", "numpy", "pandas", "scipy", "setuptools", "dask", "glob2", "protobuf", "pyplink", "scikit_learn", "tensorflow", "torch", "typing"],
       packages=['wepredict', 'nnpredict', 'spyplink'],
       test_suit='tests'
      )
